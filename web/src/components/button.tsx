7afa807103a160dd2503babbdec98267bf93eca0
Evoney Tavares <evoney.tavares@gmail.com>
2023-06-05 22:39:25 -0400

    finish web and server

--- web/src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-interface ButtonProps {
-  title: string
-}
-
-export default function Button(props: ButtonProps) {
-  return (
-    <button className="text-clip bg-black text-red-500">{props.title}</button>
-  )
-}
